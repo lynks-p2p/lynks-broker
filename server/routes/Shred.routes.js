50b46e75c4204977e6fe01ea8047d243047e70b8
Mansour <mansourk@aucegypt.edu>
2016-12-10 01:42:35 +0200

    added routes for /shreds

--- server/routes/Shred.routes.js
+++ server/routes/Shred.routes.js
@@ -1,8 +1,5 @@
 import express from 'express';
-import validate from 'express-validation';
-import paramValidation from '../../config/param-validation';
-import shredsCtrl from '../controllers/old-user.controller';
-import shredsManagerCtrl from '../controllers/ShredsManager.controller'
+import shredsManagerCtrl from '../controllers/ShredsManager.controller';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
