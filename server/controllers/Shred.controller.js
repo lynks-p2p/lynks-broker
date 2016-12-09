bd390d530fb3e44e8b12aa107fd54ecd01c2d905
James Wesley <james94@aucegypt.edu>
2016-12-10 00:12:33 +0200

    added shred controller

--- server/controllers/Shred.controller.js
+++ server/controllers/Shred.controller.js
@@ -1,14 +1,30 @@
-// uncomment this
-// import Shred from '../models/Shred.model';
+import Shred from '../models/Shred.model';
+/*create new shred. */
+function create(req, res, next) {
+  const shred = new Shred({
+    ownerId: req.body.ownerid,
+    shredSize: req.body.shredsize
+  });
 
-/**
- * Comment the function
- */
-function name(req, res, next, any, other, params) {
-  // DO IT
-
-  // dumb
-  return req + res + next + any + other + params;
+  shred.save()
+    .then(savedShred => res.json(savedShred))
+    .catch(e => next(e));
+}
+/*Load shred and append to req.*/
+function get(req, res,next,id) {
+  Shred.get(id)
+    .then((shred) => {
+      req.shred = shred;
+      return next();
+    })
+    .catch(e => next(e));
+}
+/*Delete shred*/
+function remove(req, res, next) {
+  const shred = req.shred;
+  shred.remove()
+    .then(deletedShred => res.json(deletedShred))
+    .catch(e => next(e));
 }
 
-export default { name };
+export default { create,get,remove };
