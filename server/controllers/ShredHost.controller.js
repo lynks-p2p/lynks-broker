3f37ea47b2b4a8e26f8c2dee9af302b08f4a3611
yehiahesham <yehia.hisham2012@gmail.com>
2016-12-10 01:12:07 +0200

    created ShredHost control

--- server/controllers/ShredHost.controller.js
+++ server/controllers/ShredHost.controller.js
@@ -1,14 +1,43 @@
 // uncomment this
-// import ShredHost from '../models/ShredHost.model';
+import ShredHost from '../models/ShredHost.model';
 
-/**
- * Comment the function
- */
-function name(req, res, next, any, other, params) {
-  // DO IT
+function create(req, res, next) {
 
-  // dumb
-  return req + res + next + any + other + params;
+  const newShredHost= new ShredHost({
+    shredId : req.params.shredId,
+    hostId  : req.params.hostId
+  });
+  newShredHost.save()
+    .then(savedShredHost => res.json(savedShredHost))
+    .catch(e=>next(e));
 }
 
-export default { name };
+function get(req, res, next){
+  ShredHost.findOne({shredId: req.params.shredId, hostId: req.params.hostId })
+    .then(shredhost => res.json(shredhost))
+    .catch(e=>next(e));
+}
+
+function updateShredHost(req,res,next){
+  ShredHost.findOne({shredId: req.params.shredId, hostId: req.params.hostId })
+    .then((shredhost) => {
+      shredhost.hostId = req.params.newhostId;
+      shredhost.save()
+        .then(savedShredHost => res.json(savedShredHost))
+        .catch(e => next(e));
+    })
+    .catch(e=>next(e));
+}
+
+function remove(req, res, next){ //to be completed
+  ShredHost.findOne({shredId: req.params.shredId, hostId: req.params.hostId })
+    .remove()
+    .exec()
+    .then(shredhost => {
+      return res.json(shredhost);
+    })
+    .catch(e=>next(e));
+}
+
+
+export default { create, get, updateShredHost, remove };
