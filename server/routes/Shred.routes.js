e36131d3466da4fe7ecdcae3925dfd2c8dc7a7e7
Mansour <mansourk@aucegypt.edu>
2016-12-10 01:55:11 +0200

    add comments to pass some critical args in req.body

--- server/routes/Shred.routes.js
+++ server/routes/Shred.routes.js
@@ -6,10 +6,10 @@ const router = express.Router(); // eslint-disable-line new-cap
 // NOTE TO FUTURE-SELF: Use req.body instead
 router.post('/new/:userId/:nShreds/:lastSize', shredsManagerCtrl.store);
 
-
+// req.body: {shredIds}
 router.get('/get_hosts', shredsManagerCtrl.retrieve);
 
-
+// req.body: {fileMap, [shredIds], userId}
 router.delete('/delete', shredsManagerCtrl.remove);
 
 export default router;
