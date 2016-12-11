d7b23f1d53515cab8cb4f3a773b125cab1df040a
Mansour <mansourk@aucegypt.edu>
2016-12-11 15:30:07 +0200

    reviewed User routes

--- server/routes/User.routes.js
+++ server/routes/User.routes.js
@@ -5,16 +5,31 @@ import userCtrl from '../controllers/User.controller';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-  /** POST /api/users - signup */
-router.post('/signup/:username/:capacity', validate(paramValidation.createUser), userCtrl.create);
+// NOTE TO FUTURE-SELF: validate(paramValidation.create)
 
-  /** POST /api/users - signin */
-router.post('/signin/:username', validate(paramValidation.login), userCtrl.get);
+/** POST /api/users - signup */
+// Params: username, capacity
+// Returns: User
+router.post('/signup', userCtrl.create);
 
-  //** PUT /api/users/:capacity - update_capacity */
-router.put('/update_capacity/:userId/:capacity',validate(paramValidation.updateUser), userCtrl.updateCapacity);
+/** POST /api/users - signin */
+// Expected params in res.body: userId
+// Returns: User
+router.post('/signin', userCtrl.get);
 
-  /** DELETE /api/users/:userId - leave */
-router.delete('/leave/:userId',userCtrl.remove);
+/** PUT /api/users - update_capacity */
+// Expected params in res.body: userId, capacity
+// Returns: User
+router.put('/update_capacity', userCtrl.updateCapacity);
+
+/** PUT /api/users - update_capacity */
+// Expected params in res.body: userId, fileMap
+// Returns: User
+router.put('/update_filemap', userCtrl.updateFilemap);
+
+/** DELETE /api/users/ - leave */
+// Expected params in res.body: userId, fileMap
+// Returns: User
+router.delete('/leave', userCtrl.remove);
 
 export default router;
